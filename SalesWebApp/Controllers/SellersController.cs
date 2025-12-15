6f47b92cd9d8fcd172e7a7ee3684588b23a6c951
Paulo Henrique <paulo.barreto11@hotmail.com>
2025-12-15 16:33:35 -0300

    Creating CRUD for Sellers

--- SalesWebApp/Controllers/SellersController.cs
+++ SalesWebApp/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebApp.Models;
 using SalesWebApp.Services;
 
 namespace SalesWebApp.Controllers
@@ -16,6 +17,19 @@ namespace SalesWebApp.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
